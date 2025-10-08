6e3f2ccc30ad519bc71176a0d2035765a26a40be
Antonio <6moonlight.shining9@gmail.com>
2025-10-08 15:39:29 +0300

    clean state for client-server

---
+++ src/server/command/SetFields.java
@@ -0,0 +1,15 @@
+/*package server.command;
+
+import common.Response;
+
+public class SetFields extends Command {
+    public SetFields() {
+        super("set_fields", "sets empty fields of the element", 1);
+    }
+
+    @Override
+    public Response execute(String[] args) {
+
+        return null;
+    }
+}*/
